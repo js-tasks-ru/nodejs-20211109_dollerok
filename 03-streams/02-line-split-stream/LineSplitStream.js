5694694add609cebbac659de32feec34f1a6dadb
Dollerino <93527680+Dollerino@users.noreply.github.com>
2021-11-26 16:59:12 +0300

    add solution for 2-module 2-task

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -1,15 +1,38 @@
 const stream = require('stream');
 const os = require('os');
 
+var lastElement = '';
+
 class LineSplitStream extends stream.Transform {
+
   constructor(options) {
     super(options);
   }
 
   _transform(chunk, encoding, callback) {
+    if (lastElement) {
+      chunk = lastElement + chunk;
+    }
+
+    var elements = chunk.toString().split(os.EOL);
+    lastElement = elements.pop();
+    
+    if (elements != 0) {
+      elements.forEach((element) => {
+        this.push(element);
+      });
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    if (lastElement) {
+      this.push(lastElement);
+    };
+    lastElement = '';
+
+    callback();
   }
 }
 
