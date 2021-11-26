9a3f18168cd608e9adf76d2ac3d0ad240f65e00a
Dollerino <93527680+Dollerino@users.noreply.github.com>
2021-11-26 23:43:42 +0300

    add solution for 3-module 3-task

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,10 +1,26 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+var fileData = '';
+
+function ErrorHandler(readStream, res, statusCode, resMsg) {
+  readStream ? readStream.destroy() : null;
+  statusCode ? res.statusCode = statusCode : null;
+  resMsg ? res.end(resMsg) : null;
+  fileData = '';
+}
 
 server.on('request', (req, res) => {
+  server.on('clientError', () => {
+    ErrorHandler(stream, null, null, null);
+  });
+  server.on('error', (error) => {
+    ErrorHandler(stream, res, 500, 'Some error on server.');
+  })
+
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
@@ -13,11 +29,32 @@ server.on('request', (req, res) => {
   switch (req.method) {
     case 'GET':
 
+      let stream = fs.createReadStream(filepath);
+
+      if (pathname.lastIndexOf("/") > -1) {
+        ErrorHandler(stream, res, 400, 'Do not support nesting.');
+      }
+
+      stream.on('error', (error) => {
+        if (error.code == 'ENOENT') {
+          ErrorHandler(stream, res, 404, 'File not found.');
+        } else {
+          ErrorHandler(stream, res, 500, 'Read file error.');
+        }
+      })
+
+      stream.on('data', (chunk) => {
+        fileData += chunk;
+      });
+
+      stream.on('end', () => {
+        res.end(fileData);
+        fileData = '';
+      })
       break;
 
     default:
-      res.statusCode = 501;
-      res.end('Not implemented');
+      ErrorHandler(stream, res, 501, 'Not implemented');
   }
 });
 
